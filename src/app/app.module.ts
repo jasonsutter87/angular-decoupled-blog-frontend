304c293e53fe2d12f7334abf0fd2e3799f812444
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-03-14 14:15:03 -0700

    Front end is now making http requests to my api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import { DataService } from './data.service';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+// import { PostDetailComponent } from './post-detail/post-detail.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
-    PostDetailComponent,
-    DashboardComponent
+    PostsComponent
+    // PostDetailComponent,
+    // DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // BrowserAnimationsModule,
+    HttpClientModule
   ],
+  // providers: [DataService],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Post } from '../post';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/observable/of';
+
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    readonly ROOT_URL = 'https://angular-decoupled-blog-backend.herokuapp.com'
+
+
+    posts: Observable<any>;
+    newPost: Observable<any>;
+
+    constructor(private http: HttpClient){}
+
+    // create get
+    getPosts() {
+      this.posts = this.http.get(this.ROOT_URL);
+    }
+
+    ngOnInit() {
+    }
+
+    // create post
+    createPost() {
+        let data: Post = {
+        id: 21,
+        title: 'My New Post9189281',
+        author: 'jason Pizzahead',
+        content: 'Hello 111World!!'
+      }
+      this.newPost = this.http.post<Post>(this.ROOT_URL+ '/posts', data);
+    }
+
+
 
 }
